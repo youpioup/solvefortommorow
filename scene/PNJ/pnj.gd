002aa49918807ad337cf9a3b88431679fb88e662
Jonthan <jonathan2lozere@gmail.com>
2025-02-10 17:22:55 +0100

    possibilité de changer la texture afficher dans le text box

--- scene/PNJ/pnj.gd
+++ scene/PNJ/pnj.gd
@@ -1,13 +1,16 @@
 extends Area2D
 
 @export var txt = "salut Kayou !"
+@export var img:Texture
 
 @onready var label: Label = $CanvasLayer/textBox/MarginContainer/HBoxContainer/Label
 @onready var text_box: PanelContainer = $CanvasLayer/textBox
+@onready var texture_rect = $CanvasLayer/textBox/MarginContainer/HBoxContainer/TextureRect
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Kayou":
 		label.text = txt
+		texture_rect.texture = img
 		text_box.show()
 
 
