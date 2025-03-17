a626d6581dd83625e07c2584d48d2dac6eb3c039
Jonthan <jonathan2lozere@gmail.com>
2025-03-17 16:18:12 +0100

    clean du code du pnj

--- scene/PNJ/pnj.gd
+++ scene/PNJ/pnj.gd
@@ -3,11 +3,6 @@ extends Area2D
 @export var txt:DialogueResource
 @export var img:Texture
 
-@onready var label: Label = $CanvasLayer/textBox/MarginContainer/HBoxContainer/Label
-@onready var text_box: PanelContainer = $CanvasLayer/textBox
-@onready var texture_rect = $CanvasLayer/textBox/MarginContainer/HBoxContainer/TextureRect
-@onready var intercat_label = $CanvasLayer/textBox/MarginContainer/intercatLabel
-
 var dialogue_start:bool = false
 var dialogue_pg_nb:int = 0
 
