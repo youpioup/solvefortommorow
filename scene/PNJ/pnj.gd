a98c6ee92ee5c5c19c02f0beb7aebfdc31272a44
Jonthan <jonathan2lozere@gmail.com>
2025-03-17 14:27:55 +0100

    Dialogues multi-lignes

--- scene/PNJ/pnj.gd
+++ scene/PNJ/pnj.gd
@@ -6,6 +6,14 @@ extends Area2D
 @onready var label: Label = $CanvasLayer/textBox/MarginContainer/HBoxContainer/Label
 @onready var text_box: PanelContainer = $CanvasLayer/textBox
 @onready var texture_rect = $CanvasLayer/textBox/MarginContainer/HBoxContainer/TextureRect
+@onready var intercat_label = $CanvasLayer/textBox/MarginContainer/intercatLabel
+
+var dialogue_start:bool = false
+
+func _ready():
+	if txt.size() >= 1:
+		intercat_label.show()
+	texture_rect.texture = img
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Kayou":
@@ -16,11 +24,18 @@ func _on_body_entered(body: Node2D) -> void:
 
 func show_simple_dialogue():
 	label.text = txt[0]
-	texture_rect.texture = img
 	text_box.show()
 
+func _process(delta):
+	if dialogue_start && Input.is_action_just_pressed("interagire"):
+		for i in txt.size():
+			label.text = txt[i]
+
 func show_advenced_dialogue():
-	pass
+	dialogue_start = true
+	var i = 0
+	label.text = txt[i]
+	text_box.show()
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.name == "Kayou":
